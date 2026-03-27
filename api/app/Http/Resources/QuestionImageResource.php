9fb0f249243833417bf30353c4fa7b3b8102d049
Mel-mel gwapo <jemenard082713@gmail.com>
2026-03-27 22:31:06 +0800

    backend adjustment for question API

---
+++ api/app/Http/Resources/QuestionImageResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionImageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'image_path' => config('app.url') . '/storage/' . $this->image_path,
+        ];
+    }
+}
